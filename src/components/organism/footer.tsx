d2ce6a4cd3460c9428be442a2d403eb0e701ad42
harshsingh-chauhan <harshsinghchauhan5@gmail.com>
2025-12-16 16:11:45 +0530

    add Footer and Header components with basic structure

--- src/components/organism/footer.tsx
+++ src/components/organism/footer.tsx
@@ -1,13 +1,27 @@
 export const footer = () => {
     return (
         <>
-            <footer>
-                <p>© 2025 MyWebsite. All rights reserved.</p>
-                <div>
-                    <a href="#">Privacy Policy</a> |
-                    <a href="#">Terms</a> |
-                    <a href="#">Support</a>
-                </div>
+            <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
+                <nav>
+                    <h6 className="footer-title">Services</h6>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Company</h6>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Legal</h6>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </nav>
             </footer>
         </>
     );
