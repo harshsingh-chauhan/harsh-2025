211fe7628919af5333ec08767fc8f86652825044
harshsingh-chauhan <harshsinghchauhan5@gmail.com>
2025-12-16 15:46:34 +0530

    add Footer and Header components with basic structure

--- src/components/organism/Header/Header.tsx
+++ src/components/organism/Header/Header.tsx
@@ -1,7 +1,16 @@
 export const Header = () => {
     return (
         <>
-            Header
+            <header>
+                <div className="logo">MyWebsite</div>
+                <nav>
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Services</a>
+                    <a href="#">Contact</a>
+                </nav>
+            </header>
+
         </>
     );
 };
